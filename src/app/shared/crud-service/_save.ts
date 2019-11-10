a61b8afb2248d30c0b5de22412ada1aed2321edf
Jeff Gilliland <jgilly18@gmail.com>
2019-11-10 22:06:07 +0100

    adding some comments to the DataSave

--- src/app/shared/crud-service/_save.ts
+++ src/app/shared/crud-service/_save.ts
@@ -1,10 +1,8 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
-import { catchError, tap } from "rxjs/operators";
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 
-import { DataService } from "./data.service";
-import { handleHttpError } from "./utilities";
+import { DataService } from './data.service';
+import { handleHttpError } from './utilities';
 
 @Injectable()
 export class DataSave {
@@ -16,61 +14,14 @@ export class DataSave {
         this.DS = ds;
     }
 
-    save<T>(model: T | any, objToSave?: T | any) {
-        this.DS.loadingMap[model.tableName] = true;
-
-        if (objToSave.id) { // UPDATE
-            if (this.DS.isOptimistic) {
-                this.cacheAndNotifySaved(model, objToSave);
-            }
-    
-            this.http.patch(`${this.DS.endpoint}${model.tableName}`, objToSave, this.DS.httpOptions).subscribe(
-                res => {
-                    if (!this.DS.isOptimistic) {
-                        this.cacheAndNotifySaved(model, objToSave);
-                    }
-                    this.DS.loadingMap[model.tableName] = false;
-                },
-                err => {
-                    handleHttpError(err);
-                    this.DS.loadingMap[model.tableName] = false;
-                }
-            );            
-        } else { // CREATE
-            this.http.post(`${this.DS.endpoint}${model.tableName}`, objToSave, this.DS.httpOptions).subscribe(
-                (res: any) => {
-                    Object.assign(objToSave, res);
-                    this.cacheAndNotifySaved(model, objToSave);
-                    this.DS.loadingMap[model.tableName] = false;
-                },
-                err => {
-                    handleHttpError(err);
-                    this.DS.loadingMap[model.tableName] = false;
-                }
-            );
-        }
-    }
-
-    saveObs<T>(model: T | any, objToSave?: T | any): Observable<T | T[]> {
-        if (objToSave.id) { // UPDATE
-            return this.http.patch<T[]>(`${this.DS.endpoint}${model.tableName}`, objToSave, this.DS.httpOptions).pipe(
-                catchError(handleHttpError),
-                tap((res: T[]) => {
-                    this.cacheAndNotifySaved(model, objToSave);
-                })
-            );
-        } else { // CREATE
-            return this.http.post(`${this.DS.endpoint}${model.tableName}`, objToSave, this.DS.httpOptions).pipe(
-                catchError(handleHttpError),
-                tap((res: T[] | any) => {
-                    Object.assign(objToSave, res);
-                    this.cacheAndNotifySaved(model, objToSave);
-                })
-            );
-        }
-    }
-
-    async savePromise<T>(model: T | any, objToSave: T | any): Promise<T | any> {
+    /**
+     * Save an object to the database; saving will determine if the object is new (doesn't have an id) or needs to be updated. This will determine whether or not to POST or PATCH the object.
+     * It will also detect if the incoming objToSave is an array, indicating we have a bulk update scenario, so it will split this into a create and update call with array.
+     * Special care needs to be taken to ensure the front end object receives the new id from the backend
+     * @param model The interface / class to construct the query against and build response objects from
+     * @param objToSave The front end object to be saved
+     */
+    async save<T>(model: T | any, objToSave: T | T[] | any | any[]): Promise<T | any> {
         try {
             if (Array.isArray(objToSave)) { // BULK UPDATE
                 objToSave.forEach((o,i) => o._saveId = i);
